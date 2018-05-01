5816b50b4baa1627eb12692ffca24a68e93f153b
Tobi Daada <tdaada@spacepointe.com>
2018-05-01 21:01:16 +0100

    Modify route settings

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(routes);
+app.use( '/api/v1/', routes);
 
 app.listen(PORT, () => {
     console.log(`now listening on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ import MealsController from '../controllers/MealsController';
 // Create Router
 const router = express.Router();
 
-router.get('/api/v1/meals', MealsController.getAllMeals);
+router.get('/meals', MealsController.getAllMeals);
 
 export default router;
