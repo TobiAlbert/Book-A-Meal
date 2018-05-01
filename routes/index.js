5e839ac0bd1e002ee01001c4b24a284d165b3f2a
Tobi Daada <tdaada@spacepointe.com>
2018-05-01 20:44:10 +0100

    Fix typo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import db from '../db/db';
+import MealsController from '../controllers/MealsController';
 
 // Create Router
-const router = Router();
+const router = express.Router();
 
-
+router.get('/api/v1/meals', MealsController.getAllMeals);
 
 export default router;
